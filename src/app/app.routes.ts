0074c2e22aa5bca9bf74ded33e49a8d1563705f5
Juan Pablo Leiva <juampi.leiva5@gmail,com>
2026-05-19 12:27:51 -0300

    agrego guards sprint2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { Registro } from './pages/registro/registro';
 import { Home } from './pages/home/home';
 import { QuienSoy } from './pages/quien-soy/quien-soy';
 
+import { noAuthGuard } from './guards/no-auth.guard';
+
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'login', component: Login },
-  { path: 'registro', component: Registro },
+  { path: 'login', component: Login, canActivate: [noAuthGuard] },
+  { path: 'registro', component: Registro, canActivate: [noAuthGuard] },
   { path: 'quien-soy', component: QuienSoy },
   { path: '**', redirectTo: '' }
 ];
